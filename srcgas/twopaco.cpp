ddd7c9e75dda51697f3e24cd529a5507dd99ef61
Gaspare Ferraro <ferraro@gaspa.re>
2018-05-28 15:09:22 +0200

    filter junctions and two-pass

--- srcgas/twopaco.cpp
+++ srcgas/twopaco.cpp
@@ -91,27 +91,44 @@ void filterJunction(const std::vector<std::string>& S, int k,
     std::function<bool(const std::string&)> contains,
     std::set<std::pair<int,int>>& C)
 {
+
+  std::cerr << "FilterJunction insert k-mers in set" << std::endl;
   for(std::pair<int,int> c : C)
   {
-    // TODO
+    // For each candidates insert the 2 corresponding (k+1)-mers 
+    insert(S[c.first].substr(c.second-1, k+1));
+    insert(S[c.first].substr(c.second, k+1));
   }
 
   // Candidate to remove
   std::vector<std::pair<int,int>> toRemove;
 
   // Find which k-mers are junctions
+  std::cerr << "FilterJunction querying the set" << std::endl;
   for(std::pair<int,int> c : C)
   {
     int in=0, out=0;
+    std::string current = S[c.first].substr(c.second, k);
 
-    // TODO
+    // Count in-edge
+    in += contains("A"+current);
+    in += contains("C"+current);
+    in += contains("G"+current);
+    in += contains("T"+current);
 
+    // Count out-edge
+    out += contains(current+"A");
+    out += contains(current+"C");
+    out += contains(current+"G");
+    out += contains(current+"T");
+    
     // Not a junctions, insert to the remotion list
     if( in == 1 && out == 1 )
         toRemove.push_back(c);
   }
 
   // Remove false candidates
+  std::cerr << "FilterJunction remove false candidates" << std::endl;
   for(std::pair<int,int> r : toRemove)
     C.erase(r);
 }
@@ -147,8 +164,27 @@ std::set<std::string> TwoPaCo(const std::vector<std::string>& S, int k, int b, i
 {
   std::set<std::string> junctions;
 
-  // TODO 
-  
+  // Create partition using roundSplitting
+  std::cerr << "TwoPaCo create partitions" << std::endl;
+  std::vector<std::pair<int,int>> chunks = roundSplitting(S, k, l);
+
+  // Iteratore over chunks and analyze them separately
+  for(int i=0; i<l; i++)
+  {
+    std::cerr << "TwoPaCo filter chunk #" << i << std::endl;
+    std::set<std::pair<int,int>> candidates;
+    
+    // Create set of junction candidates
+    // TODO
+
+    // Find real junctions
+    TwoPass(S, k, b, candidates);
+
+    // Merge them in the final set of junctions
+    for(auto junction : candidates)
+      junctions.insert( S[junction.first].substr(junction.second, k) );
+  }
+
   return junctions;
 }
 
